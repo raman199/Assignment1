6cdf379b51de53dd1440e22f8c2a800c2f5a1d01
Ramandeep Kaur <pawarraman9@gmail.com>
2019-09-25 21:29:51 -0400

    version v1

--- Assignment1/ViewController.swift
+++ Assignment1/ViewController.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var m_operand = ""
+   
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultLabel.text="0"
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func ClickButton(_ sender: UIButton) {
+        var calculatorButton = sender.titleLabel?.text
+
+        switch(calculatorButton)
+        {
+            
+        case ".":
+            m_operand += calculatorButton!
+            break
+        default:
+            m_operand += calculatorButton!
+            
+        }
+        
+        resultLabel.text=m_operand
+    }
+        
+    
+  
 
 
 }
